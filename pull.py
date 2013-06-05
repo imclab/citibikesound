bf295859b2f9efcff39c2c2b2bd0cbca36d4c337
brianhouse <brian.house@gmail.com>
2013-06-04 23:05:15 -0400

    initial commit of scraping script

---
+++ pull.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+"""
+http://appservices.citibikenyc.com/data2/stations.php
+
+http://appservices.citibikenyc.com/data2/stations.php?updateOnly=true
+
+http://appservices.citibikenyc.com/v1/helmet/list
+
+http://appservices.citibikenyc.com/v1/branch/list
+
+TODO: make bucket
+
+
+"""
+
+import time, os
+from housepy import config, log, net
+from s3_mp_upload import main
+
+ENDPOINT = "http://appservices.citibikenyc.com/data2/stations.php"
+
+filename = "%s.txt" % str(time.time()).split('.')[0]
+
+log.info("----- grab attempt")
+
+try:
+    net.grab(ENDPOINT, filename)
+except Exception as e:
+    log.error(log.exc(e))
+    exit()
+
+log.info("got %s, pushing..." % filename)    
+
+try:
+    main(filename, filename)
+except Exception as e:
+    log.error(log.exc(e))
+    exit()
+
+log.info("upload complete")
+
+os.remove(filename)
+

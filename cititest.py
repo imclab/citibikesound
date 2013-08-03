23a6eae6e84318c894c38f62cecde48b734369ee
brianhouse <brian.house@gmail.com>
2013-08-02 20:47:58 -0600

    built out more efficient sqlite storage schema

--- cititest.py
+++ cititest.py
@@ -7,26 +7,58 @@ tones vary per stations, via some map
 313 current stations; 626 sounds
 
 at each minute, we have a dict, station_id to tone
-{72: -1, 72: 0, 72: 1}
+{72: -1, 73: 0, 74: 1}, etc
+
+ok, so this can pull the endpoints directly
+
 
 """
 
-from housepy import config, log
-import json, sys, model
+from housepy import config, log, net
+import json, sys, model, os
 
-filename = sys.argv[1]
-log.info("Parsing %s" % filename)
+ENDPOINT = "http://appservices.citibikenyc.com/data2/stations.php"
 
-try:
-    with open(filename) as f:
-        data = json.loads(f.read())
-except Exception as e:
-    log.error(log.exc(e))
-    exit()
+def handle(t, data):
+    stations = model.fetch_stations()
+    events = {}
+    for s in data['results']:
+        try:
+            if s['id'] not in stations:
+                model.insert_station(s['id'], s['longitude'], s['latitude'], t, s['availableBikes'])
+                continue
+            station = stations[s['id']]
+            if s['availableBikes'] != station['bikes'] and t > station['t']:
+                json.dumps(s, indent=4)
+                model.update_station(s['id'], t, s['availableBikes'])
+                events[s['id']] = s['availableBikes'] - station['bikes']
+        except Exception as e:
+            log.error(log.exc(e))
+            print(station)
+    model.insert_beat(t, events)
 
-stations = model.fetch_stations()
-for station in data['results']:
-    print(json.dumps(station, indent=4))
-    if station['id'] not in stations:
-        model.insert_station(station['id'], station['longitude'], station['latitude'], station['availableBikes'], station['availableDocks'])
-        continue
+
+if len(sys.argv) > 1:
+    for filename in os.listdir("files"):
+        log.info("Parsing %s" % filename)
+        t = int(filename.split('.')[0])
+        if model.check_t(t):
+            log.info("skipping...")
+            continue
+        try:
+            with open("files/" + filename) as f:
+                data = json.loads(f.read())
+        except Exception as e:
+            log.error(log.exc(e))
+            exit()
+        handle(t, data)
+else:
+    log.info("Grabbing from endpoint...")
+    try:
+        t = time.time()
+        data = json.loads(net.read(ENDPOINT))
+    except Exception as e:
+        log.error(log.exc(e))
+        exit() 
+    handle(t, data)
+
--- model.py
+++ model.py
@@ -9,13 +9,20 @@ db = connection.cursor()
 
 def init():
     try:
-        db.execute("CREATE TABLE IF NOT EXISTS stations (station_id INTEGER, lon REAL, lat REAL, bikes INT, docks INT)")
+        db.execute("CREATE TABLE IF NOT EXISTS stations (station_id INTEGER, lon REAL, lat REAL, t INT, bikes INT)")
         db.execute("CREATE UNIQUE INDEX IF NOT EXISTS stations_station_id ON stations(station_id)")
+        db.execute("CREATE TABLE IF NOT EXISTS beats (t INT, events TEXT)")
+        db.execute("CREATE UNIQUE INDEX IF NOT EXISTS beats_t ON beats(t)")
     except Exception as e:
         log.error(log.exc(e))
         return
     connection.commit()
 init()
+
+def check_t(t):
+    db.execute("SELECT t FROM beats WHERE t>=?", (t,))
+    result = db.fetchone()
+    return result is not None
 
 def fetch_stations():
     stations = {}
@@ -25,8 +32,22 @@ def fetch_stations():
         stations[station['station_id']] = dict(station)
     return stations
 
-def insert_station(station_id, lon, lat, bikes, docks):
-    db.execute("INSERT INTO stations (station_id, lon, lat, bikes, docks) VALUES (?, ?, ?, ?, ?)", (station_id, lon, lat, bikes, docks))
+def insert_station(station_id, t, lon, lat, bikes):
+    db.execute("INSERT INTO stations (station_id, lon, lat, t, bikes) VALUES (?, ?, ?, ?, ?)", (station_id, lon, lat, t, bikes))
     log.info("added station_id %s" % station_id)
+    connection.commit()
+
+def update_station(station_id, t, bikes):
+    db.execute("UPDATE stations SET t=?, bikes=? WHERE station_id=?", (t, bikes, station_id))
+    log.info("updated station_id %s" % station_id)
+    connection.commit()
+
+def insert_beat(t, events):
+    num = len(events)
+    if num == 0:
+        return
+    events = json.dumps(events)
+    db.execute("INSERT INTO beats (t, events) VALUES (?, ?)", (t, events))
+    log.info("added events for %s stations" % num)
     connection.commit()
 
